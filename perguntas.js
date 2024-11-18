4630f88dc704d8186cbe1fe0e3570312e4f279e2
Santana070 <joaosantana0310@gmail.com>
2024-11-18 09:06:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Futebol',
+    'Qual o Maior Time do Brasil?',
+    'O Santos Futebol Clube é o Maior'
+)
+
+criaCartao(
+    'Futebol',
+    'Palmeiras tem Mundial?',
+    'Nunca teve'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem realmente mereceu ganhar a Bola de Ouro?',
+    'Vinícius Júnior'
+)
+
+criaCartao(
+    'Futebol',
+    'Onde o Cristiano Ronaldo joga atualmente?',
+    'Atualmente no Al-Nassr'
+)
+
+criaCartao(
+    'Futebol',
+    'Onde o Neymar Jr. joga atualmente?',
+    'Atualmente no Al-Hilal'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o Pior Time do Brasil?',
+    'O Corinthians'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantos Brasileiros ganharam a Bola de Ouro?',
+    'Cinco jogadores brasileiros'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o Maior Jogador de Futebol?',
+    'Pelé'
+)
